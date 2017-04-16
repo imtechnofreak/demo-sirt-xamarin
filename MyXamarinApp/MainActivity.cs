9f4978bfdbff8abe63e7725a252f04bb7964c7ed
Rohit Jain <rohit_j@live.com>
2017-04-17 01:45:22 +0530

    Add project files.

---
+++ MyXamarinApp/MainActivity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace MyXamarinApp
+{
+    [Activity(Label = "MyXamarinApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        EditText inputbill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            inputbill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += CalculateButton_Click;
+            
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            string text = inputbill.Text;
+            var bill = double.Parse(text);
+            var tip = bill * 0.15;
+            var total = bill + tip;
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
+
+        }
+    }
+}
+
